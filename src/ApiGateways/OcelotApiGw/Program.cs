c8bb086ad6e277058c62a739ebcea1f4e7864352
kelvinlam <kelvincoder@gmail.com>
2023-11-04 13:38:58 +0700

    Configure and Containerize Ocelot  Gateway

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,5 +1,6 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Host.ConfigureAppConfiguration((context, cfg) =>
@@ -15,7 +16,8 @@ builder.Host.ConfigureLogging(logging =>
     logging.AddDebug();
 });
 
-builder.Services.AddOcelot();
+builder.Services.AddOcelot()
+    .AddCacheManager(settings => settings.WithDictionaryHandle());
   
 var app = builder.Build();
 
